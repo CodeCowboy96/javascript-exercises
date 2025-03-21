18cc5fa85d2887b80d7ea07e5052123c8d2cd07a
Tyler <179739422+CodeCowboy96@users.noreply.github.com>
2025-03-21 11:10:00 -0500

    Complete multiple practice exercises.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
+//Add 2 + 2
+const add = function(a,b) {
+	return a + b
+};
+
+//subtract 
+const subtract = function(c,d) {
+	return c-d
+};
+
+const sum = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(mArr) {
+  return mArr.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const sum = function() {
-	
+const power = function(x,y) {
+	return x ** y
 };
 
-const multiply = function() {
-
+const factorial = function(num) {
+	if (num < 0) 
+    return -1;
+else if (num == 0) 
+  return 1;
+else {
+  return (num * factorial(num - 1));
+  }
 };
 
-const power = function() {
-	
-};
 
-const factorial = function() {
-	
-};
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+//take string text input and turn it into an array of individual strings for each letter
 
+
+//sort and compare array forwards and backwards
+
+
+
+
+
+const palindromes = function (saying) {
+   saying.toCharArray()
 };
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
