219886df2a92b287e1792519556e8382294b1c7a
Tyler <179739422+CodeCowboy96@users.noreply.github.com>
2025-03-21 16:42:35 -0500

    Complete js array exercises.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
 
-};
-
+const findTheOldest = function(people) {
+   const oldest = people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = (oldestPerson.yearOfDeath || new Date().getFullYear()) - oldestPerson.yearOfBirth;
+    const currentAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+    return currentAge > oldestAge ? currentPerson : oldestPerson;
+    }, people[0]);
+    return oldest;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
